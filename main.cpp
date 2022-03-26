f4b8064805d589bce79fedb82d8245b286ee040b
Jimmy Fang <yixiaof2@illinois.edu>
2022-03-26 04:02:12 +0000

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
 // main.cpp
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    double latitude;
+    double longtitude;
+    string airport_name;
+
+    // Open a new file to perform read operation line by line
+    fstream newfile;
+    newfile.open("airport.txt", ios::in);
+    if (newfile.is_open() == true)
+    {
+        string first_line;
+        getline(newfile, first_line);
+
+        cout << first_line << endl;
+        newfile.close(); 
+    }
+}
