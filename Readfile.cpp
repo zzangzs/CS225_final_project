71cc4b800ba0170b340550ceab44649a44223024
Jimmy Fang <yixiaof2@illinois.edu>
2022-03-30 03:01:19 +0000

    Update Readfile

--- Readfile.cpp
+++ Readfile.cpp
@@ -28,15 +28,30 @@ Readfile::~Readfile()
 }
 
 
+/** 
+    Helper function to check whether a string is a number. 
+
+    @param string The string that needs to be checked
+    @return true if the string is a number, otherwise false
+**/
+bool Readfile::isNumber(const string& str)
+{
+    for (char const &c : str) 
+    {
+        if (std::isdigit(c) == 0) return false;
+    }
+    return true;
+}
+
 /**
-    The readfile_airport function accepts a vector storing pointers of Airport objects, and 
+    The readfile_airport function accepts a vector storing Airport objects, and 
     it reads every line of the input file (airport database). When reading every line, it will check the validity
     of the data and construct a corresponding Airport object. If every entry is valid, the newly
     constructed Airport object will be pushed into 'airport_vec', otherwise not. 
 
-    @param airport_vec The vector that stores pointers of every Airport object
+    @param airport_vec The vector that stores every Airport object
 **/
-void Readfile::readfile_airport(vector<Airport*> & airport_vec)
+void Readfile::readfile_airport(vector<Airport> & airport_vec)
 {
     // Open a new file to perform read operation line by line
     fstream newfile;
@@ -55,14 +70,14 @@ void Readfile::readfile_airport(vector<Airport*> & airport_vec)
 
 
 /**
-    The readfile_routes function accepts a vector storing pointers of Route struct, and 
+    The readfile_routes function accepts a vector storing Route struct, and 
     it reads every line of the input file (route database). When reading every line, it will check the validity
     of the data and instantiate a corresponding Route struct. If every entry is valid, the newly
     constructed Route struct will be pushed into 'routes_vec', otherwise not. 
 
-    @param routes_vec The vector that stores pointers of every Route struct
+    @param routes_vec The vector that stores every Route struct
 **/
-void Readfile::readfile_routes(vector<Route*> & routes_vec)
+void Readfile::readfile_routes(vector<Route> & routes_vec)
 {
     // Open a new file to perform read operation line by line
     fstream newfile;
@@ -84,10 +99,10 @@ void Readfile::readfile_routes(vector<Route*> & routes_vec)
     parse_correct_airport is the helper function that parses strings from every line of the file, and
     checks for data validity. 
 
-    @param airport_vec The vector that stores pointers of every Airport object
+    @param airport_vec The vector that stores every Airport object
     @param s Every line of the input file
 **/
-void Readfile::parse_correct_airport(string s, vector<Airport*> & airport_vec)
+void Readfile::parse_correct_airport(string s, vector<Airport> & airport_vec)
 {
     // Construct an airport object
     Airport *airport = new Airport();
@@ -141,6 +156,18 @@ void Readfile::parse_correct_airport(string s, vector<Airport*> & airport_vec)
             airport -> setCountry(substr);
         }
 
+        if (count == 4)
+        {
+            // Add IATA (IATA can be \N if not assigned)
+            airport -> setIATA(substr);
+        }
+
+        if (count == 5)
+        {
+            // Add ICAO (ICAO can be \N if not assigned)
+            airport -> setICAO(substr);
+        }
+
         if (count == 6)
         {
             // Check for valid latitude
@@ -189,10 +216,22 @@ void Readfile::parse_correct_airport(string s, vector<Airport*> & airport_vec)
         /* airport_vec.size() is the actual index refers to the airport,
             which should equal to the airport assigned id. (not the original
             one if there exist invalid airports)
+
+            The actual assigned airport id is 'airport_vec.size()'
         */
         airport -> setId(airport_vec.size());
         id_change[orig_id] = airport_vec.size();
-        airport_vec.push_back(airport);
+
+        // Set the two maps (IATA -> ID AND ICAO -> ID)
+        if (airport -> getIATA() != "")
+        {
+            iata_id[airport -> getIATA()] = airport -> getID();
+        }
+        if (airport -> getICAO() != "")
+        {
+            icao_id[airport -> getICAO()] = airport -> getID();
+        }
+        airport_vec.push_back(*airport);
     }
     else
     {
@@ -205,10 +244,10 @@ void Readfile::parse_correct_airport(string s, vector<Airport*> & airport_vec)
     parse_correct_routes is the helper function that parses strings from every line of the file, and
     checks for data validity. 
 
-    @param routes_vec The vector that stores pointers of every Route struct
+    @param routes_vec The vector that stores every Route struct
     @param s Every line of the input file
 **/
-void Readfile::parse_correct_routes(string s, vector<Route*> & routes_vec)
+void Readfile::parse_correct_routes(string s, vector<Route> & routes_vec)
 {
     // Instantiate a Route struct
     Route *route = new Route;
@@ -216,11 +255,19 @@ void Readfile::parse_correct_routes(string s, vector<Route*> & routes_vec)
     stringstream s_stream(s);
     int count = 0;
     bool route_valid = true;
+    string source;
+    string dest;
     while(s_stream.good())
     {
         // 'substr' is the string of every parse (different portions of the line)
         string substr;
         getline(s_stream, substr, ',');
+
+        if (count == 2)
+        {
+            // Read source Airport IATA or ICAO
+            source = substr;
+        }
         
         if (count == 3)
         {
@@ -230,7 +277,30 @@ void Readfile::parse_correct_routes(string s, vector<Route*> & routes_vec)
                 route_valid = false;
                 break;
             }
-            route -> startID = id_change[std::stoul(substr)];
+            else if (isNumber(substr) == 0)
+            {
+                // Find the corresponding airport ID
+                if (source.length() == 3)
+                {
+                    string find = "\"" + source + "\"";
+                    route -> endID = iata_id[find];
+                }
+                else
+                {
+                    string find = "\"" + source + "\"";
+                    route -> endID = icao_id[find];
+                }
+            }
+            else
+            {
+                route -> startID = id_change[std::stoul(substr)];
+            }
+        }
+
+        if (count == 4)
+        {
+            // Read destination Aiport IATA or ICAO
+            dest = substr;
         }
 
         if (count == 5)
@@ -241,7 +311,24 @@ void Readfile::parse_correct_routes(string s, vector<Route*> & routes_vec)
                 route_valid = false;
                 break;
             }
-            route -> endID = id_change[std::stoul(substr)];
+            else if (isNumber(substr) == 0)
+            {
+                // Find the corresponding airport ID
+                if (dest.length() == 3)
+                {
+                    string find = "\"" + dest + "\"";
+                    route -> endID = iata_id[find];
+                }
+                else
+                {
+                    string find = "\"" + dest + "\"";
+                    route -> endID = icao_id[find];
+                }
+            }
+            else
+            {
+                route -> endID = id_change[std::stoul(substr)];
+            }
         }
         count ++;
     }
@@ -249,7 +336,8 @@ void Readfile::parse_correct_routes(string s, vector<Route*> & routes_vec)
     // Determine whether the route should be stored
     if (route_valid == true)
     {
-        routes_vec.push_back(route);
+        // cout << "Push_route" << endl;
+        routes_vec.push_back(*route);
     }
     else
     {
--- Readfile.h
+++ Readfile.h
@@ -15,13 +15,21 @@ class Readfile
             It maps the original IDs from the input file to the actual assigned IDs
         */
         map<unsigned int, unsigned int> id_change;
-        void parse_correct_airport(string s, vector<Airport*> & airport_vec);
-        void parse_correct_routes(string s, vector<Route*> & routes_vec);
+        /*
+            The following two maps are used to map IATA and ICAO codes to the airport's
+            unique ID. When the airport ID is missing in the original routes file, these
+            two maps can be used to find the missing ID. 
+        */
+        map<string, unsigned int> iata_id;
+        map<string, unsigned int> icao_id;
+        void parse_correct_airport(string s, vector<Airport> & airport_vec);
+        void parse_correct_routes(string s, vector<Route> & routes_vec);
+        bool isNumber(const string& str);
 
     public:
         Readfile();
         ~Readfile();
-        void readfile_airport(vector<Airport*> & airport_vec);
-        void readfile_routes(vector<Route*> & routes_vec);
+        void readfile_airport(vector<Airport> & airport_vec);
+        void readfile_routes(vector<Route> & routes_vec);
 };
 
