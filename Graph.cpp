96fa4ef7f2696c867ff45ebe690cb297cdcfb4db
Zisu Jiang <zisuj2@siebl-0224-08.ews.illinois.edu>
2022-04-15 19:14:53 -0500

    Modify Graph index from 0

--- Graph.cpp
+++ Graph.cpp
@@ -13,7 +13,7 @@ using std::vector;
 using std::pair;
 using std::cout;
 using std::endl;
-using std::priority_queue;
+//using std::priority_queue;
 
 Graph::Graph(){
 
@@ -39,9 +39,9 @@ Graph::Graph(const vector<Route> & routes, const vector<Airport> & airports)
     }
     
     // build adjacency list
-    for (size_t row = 1; row < numAirports; row++)
+    for (size_t row = 0; row < numAirports; row++)
     {
-        for (size_t col = 1; col < numAirports; col++)
+        for (size_t col = 0; col < numAirports; col++)
         {
             pair<unsigned,unsigned> IDs = make_pair(airports[row].getID(), airports[col].getID());
             bool isIn = routesMap_.find(IDs) != routesMap_.end();
@@ -58,15 +58,15 @@ Graph::Graph(const vector<Route> & routes, const vector<Airport> & airports)
 
 void Graph::printGraph()
 {  
-    for (size_t i = 1; i < airports_ptr_->size(); i++)
+    for (size_t i = 0; i < airports_ptr_->size(); i++)
     {
-        cout << "Airport " << airports_ptr_->at(i).getID() << " is adjacent to: " << endl;
-        for (size_t j = 1; j < airports_ptr_->size(); j++)
+        cout << "Airport " << airports_ptr_->at(i).getID() +1 << " is adjacent to: " << endl;
+        for (size_t j = 0; j < airports_ptr_->size(); j++)
         {
 
             if (adj_[i][j]!=0)
             {
-                cout << "    Airport" << airports_ptr_->at(j).getID() << " with a distance of " << adj_[i][j] << endl;
+                cout << "    Airport " << airports_ptr_->at(j).getID() + 1 << " with a distance of " << adj_[i][j] << endl;
             }
             
         }
@@ -78,56 +78,56 @@ void Graph::printGraph()
 //Dijkstra: find the shortest path from one airport to the other
 //input: adjMatrix, departure airport id, destination airport id
 //output: vector of airport id representing the path
-vector<unsigned> Graph::Dijkstra(unsigned int departure, unsigned int destination){
-    //initialize distance matrix, previous node matrix, and visited matrix
-    vector<double> d; //distance matrix
-    vector<unsigned> p; //previous node matrix
-    vector<bool> visited; //visited matrix
-    double inf = std::numeric_limits<double>::max();
+// vector<unsigned> Graph::Dijkstra(unsigned int departure, unsigned int destination){
+//     //initialize distance matrix, previous node matrix, and visited matrix
+//     vector<double> d; //distance matrix
+//     vector<unsigned> p; //previous node matrix
+//     vector<bool> visited; //visited matrix
+//     double inf = std::numeric_limits<double>::max();
 
-    for (unsigned i = 0; i < numAirports; i++){
-        d.push_back(inf);
-        p.push_back(NULL);
-        visited.push_back(false);
-    }
-    d[departure] = 0;
+//     for (unsigned i = 0; i < numAirports; i++){
+//         d.push_back(inf);
+//         p.push_back(NULL);
+//         visited.push_back(false);
+//     }
+//     d[departure] = 0;
 
-    //initialize the priority queue
-    priority_queue<unsigned> pq;
-    pq.push(departure);
+//     //initialize the priority queue
+//     priority_queue<unsigned> pq;
+//     pq.push(departure);
 
-    while(pq.top() != destination){
-        unsigned cur = pq.pop();
-        for (unsigned i = 0; i < numAirports; i++){//for neighbor in current_node's neighbors
-            if (adj_[cur][i] != 0 && !visited[i]){ //push unvisisted neighbors to pq
-                if ((d[cur] + adj_[cur][i]) < d[i]){ //if cost is cheap
-                    d[i] = d[cur] + adj_[cur][i]; //update its neighbor's distance
-                    p[i] = cur; //update previous node
-                }
-                pq.push(i); //push neighbors to pq
-            } 
-        }
-        visited[cur] = true;
-    }
+//     while(pq.top() != destination){
+//         unsigned cur = pq.pop();
+//         for (unsigned i = 0; i < numAirports; i++){//for neighbor in current_node's neighbors
+//             if (adj_[cur][i] != 0 && !visited[i]){ //push unvisisted neighbors to pq
+//                 if ((d[cur] + adj_[cur][i]) < d[i]){ //if cost is cheap
+//                     d[i] = d[cur] + adj_[cur][i]; //update its neighbor's distance
+//                     p[i] = cur; //update previous node
+//                 }
+//                 pq.push(i); //push neighbors to pq
+//             } 
+//         }
+//         visited[cur] = true;
+//     }
 
-    //extract path from previous
-    vector<unsigned> path; //from departure to destination order
-    stack<unsigned> s;
-    double total_dist; //use to calculate total distance 
+//     //extract path from previous
+//     vector<unsigned> path; //from departure to destination order
+//     stack<unsigned> s;
+//     double total_dist; //use to calculate total distance 
 
-    unsigned temp = destination;
-    while(p[temp] != departure){
-        s.push(temp);
-        total_dist += adj_[p[temp]][temp]; //use to calculate total distance 
-        temp = p[temp];
-    }
-    s.push(departure);
+//     unsigned temp = destination;
+//     while(p[temp] != departure){
+//         s.push(temp);
+//         total_dist += adj_[p[temp]][temp]; //use to calculate total distance 
+//         temp = p[temp];
+//     }
+//     s.push(departure);
 
-    while(!s.empty()){
-        unsigned temp = s.pop();
-        path.push_back(temp);
-    }
+//     while(!s.empty()){
+//         unsigned temp = s.pop();
+//         path.push_back(temp);
+//     }
 
-    return path;
-}
+//     return path;
+// }
 
